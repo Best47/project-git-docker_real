d0b2d2e50cad85df824f0b41d24ff7abce2b4e1c
Best47 <chxtxek9@gmail.com>
2025-03-07 15:22:37 +0700

    Update student name and server log message

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const app = express();
 const port = 3099;
 
 app.get("/finalTest", (req, res) => {
-  res.send("korkiat somha 6604305001317<br> thanapat 6604305001329");
+  res.send("korkiat somha 6604305001317<br> thanapat hwannun 6604305001329");
 });
 
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}/finalTest`);
 
 });
